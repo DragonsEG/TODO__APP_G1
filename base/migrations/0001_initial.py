c83d3283278fc1b7b0ad7fb5f50de42b5b72177d
Radwan <tara02664@gmail.com>
2023-09-13 04:34:02 +0300

    Hello <=>

--- base/migrations/0001_initial.py
+++ base/migrations/0001_initial.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name='Task',
+            
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=250)),
@@ -24,8 +25,10 @@ class Migration(migrations.Migration):
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
+            
             options={
                 'ordering': ['complete'],
             },
-        ),
+            
+        )
     ]
